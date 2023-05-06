5c563d36fe8528ad1ae302e18f88d43cb6c533e0
rldnd <gi981226@gmail.com>
2023-05-07 00:00:20 +0900

    feat(dio): custom interceptor onError

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -14,14 +14,14 @@ class CustomInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    if (options.headers[ACCESS_TOKEN_KEY]) {
+    if (options.headers[ACCESS_TOKEN_KEY] == true) {
       options.headers.remove(ACCESS_TOKEN_KEY);
 
       final token = await storage.read(key: ACCESS_TOKEN_KEY);
       options.headers.addAll({'authorization': 'Bearer $token'});
     }
 
-    if (options.headers[REFRESH_TOKEN_KEY]) {
+    if (options.headers[REFRESH_TOKEN_KEY] == true) {
       options.headers.remove(REFRESH_TOKEN_KEY);
 
       final token = await storage.read(key: REFRESH_TOKEN_KEY);
@@ -29,5 +29,40 @@ class CustomInterceptor extends Interceptor {
     }
 
     return super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      try {
+        final dio = Dio();
+        final response = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(headers: {'authorization': 'Bearer $refreshToken'}),
+        );
+
+        final accessToken = response.data['accessToken'];
+        final options = err.requestOptions;
+
+        options.headers.addAll({'authorization': 'Bearer $accessToken'});
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        final reResponse = await dio.fetch(options);
+        return handler.resolve(reResponse);
+      } on DioError catch (e) {
+        return handler.reject(e);
+      }
+    }
+
+    return super.onError(err, handler);
   }
 }
